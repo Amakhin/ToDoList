e2f6cc3d16c3a96e326539f0d397cb2a01649969
Amakhin <amakhinruslan@gmail.com>
2019-11-22 12:58:11 +0200

    Basic data model methods

--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+var toDoItems: [String] = ["Позвонить маме", "Написать приложение"]
+
+func addItem(nameItem: String) {
+    toDoItems.append(nameItem)
+    saveData()
+}
+
+func removeItem(at index: Int) {
+    toDoItems.remove(at: index)
+    saveData()
+}
+
+func saveData() {
+    
+}
+
+func loadData() {
+    
+}
